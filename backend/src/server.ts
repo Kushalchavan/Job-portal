6bb627b6d3554d73439d758860db13d83dfd5b51
Kushalchavan <kushalchavan2002@gmail.com>
2026-02-26 23:18:06 +0530

    fix: downgrade to prisma v6 and switch to classic datasource configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import type { Request, Response } from "express";
 import { errorHandler } from "./middlewares/error.middleware";
 import morgan from "morgan";
 import { env } from "./config/env";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 
@@ -10,9 +10,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 // All routes here
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello world");
-});
+app.use("/api/v1/auth", authRoutes);
 
 // Error handler
 app.use(errorHandler);
