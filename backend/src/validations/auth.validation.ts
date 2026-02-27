25e8dc8629ab5b4458ca64aca55d8c3d16de3312
Kushalchavan <kushalchavan2002@gmail.com>
2026-02-27 23:44:21 +0530

    feat: add zod validation to authentication

--- backend/src/validations/auth.validation.ts
+++ backend/src/validations/auth.validation.ts
@@ -1,12 +1,13 @@
 import * as z from "zod";
 
-export const registerScheam = z.object({
+export const registerSchema = z.object({
   name: z.string().min(3, "Name must be atleast 3 characters"),
   email: z.string().email("Invalid email format"),
   password: z.string().min(6, "Password must be at least 6 characters"),
+  role: z.enum(["USER", "RECRUITER", "ADMIN"]),
 });
 
 export const loginSchema = z.object({
   email: z.string().email(),
-  pasword: z.string().min(6),
+  password: z.string().min(6),
 });
